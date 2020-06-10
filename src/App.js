b3fd5b0fd3d8dac0f3c3865cb9cc9d56a7b450a4
Ken Charette <charettekenneth@gmail.com>
2020-06-10 15:46:27 -0400

    Added in logic for error text and changing border colors

--- src/App.js
+++ src/App.js
@@ -1,44 +1,104 @@
 import React, { useState, useEffect } from 'react'
 import './App.css'
 import './styles/app.css'
+import Header from './Header'
 
 const App = () =>{
   const [population, setPopulation] = useState(2)
   const [cities, setCities] = useState(0)
   const [gold, setGold] = useState(0)
   const [border, setBorder] = useState('')
+  const [farmBorder, setFarmBorder] = useState('')
+  const [farm, setFarm] = useState(0)
+  const [mineBorder, setMineBorder] = useState('')
+  const [mine, setMine] = useState(0)
+  const [buttonDisable, setButtonDisable] = useState(false)
+  const [message, setMessage] = useState('')
 
   useEffect(() => {
     const interval = setInterval(() => {
       setGold(gold => gold + 1)
-    }, 800)
 
-    if (population >= 10 && gold >= 10){
+      if (farm) {
+        setGold(gold => gold + 2)
+      }
+      if (mine){
+        setGold(gold => gold + 3)
+      }
+    }, 500)
+
+    if (population >= 10 && gold >= 20){
       setBorder('border-green-500')
+    } else if (population < 10 || gold < 10){
+      setBorder('border-red-500')
+    } else {
+      setBorder('border-red-500')
     }
 
-    if (population < 10 || gold < 10){
-      setBorder('border-red-500')
+    // increase gold and city amount
+    if (gold >= 15 && population >= 30 && cities >= 1){
+      setFarmBorder('border-green-500')
+    } else {
+      setFarmBorder('border-red-500')
+    }
+
+    // increase gold and city amount
+    if (gold >= 30 && cities >= 2){
+      setMineBorder('border-green-500')
+    } else {
+      setMineBorder('border-red-500')
+    }
+
+    if (gold < 0){
+      setButtonDisable(true)
+      setMessage(`You don't have enough gold!`)
+    } else if (gold > 0) {
+      setButtonDisable(false)
+      setMessage(``)
     }
 
     return () => clearInterval(interval)
 
-  },[gold])
+  },[gold, farm])
 
   const handlePopulation = () => {
     setPopulation(population + 1)
   }
 
-  const handleCreateSettlement = () => {
+  const handleCreateCity = () => {
     if (population >= 10 && gold >= 10) {
       setCities(cities + 1)
       setPopulation(population - 5)
-      setGold(gold - 5)
+      setGold(gold - 20)
     }
   }
 
+  const handleCreateFarm = () => {
+    setFarm(farm => farm + 1)
+    setGold(gold - 15)
+
+    setInterval(() => {
+      setGold(gold => gold + 2)
+    }, 4000)
+  }
+
+
+  const handleCreateMine = () => {
+    setMine(mine => mine + 1)
+    setGold(gold - 30)
+
+    setInterval(() => {
+      setGold(gold => gold + 3)
+    }, 2000)
+
+  }
+
+
   return (
+    <div>
+      <Header />
     <div className='p-10 text-center'>
+      
       <div className='m-auto'>
         <img style={{margin: 'auto', maxWidth: '20%'}} src='https://gamepedia.cursecdn.com/civ6_gamepedia_en/9/92/Icon_Gold.png?version=5a0fdb33eab59cf79da6d07bf6b869e1' alt='gold'/>
         <h2> Gold {gold} </h2>
@@ -52,22 +112,30 @@ const App = () =>{
       <div>
         <img style={{ margin: 'auto', height: '40px', width: '40px'}}src='https://gamepedia.cursecdn.com/civ6_gamepedia_en/1/1f/Icon_building_library.png?version=a37f5bb6db3813332168a160b93f3f0a' alt='building'/>
       <h2> Cities {cities} </h2>
-        <button className={`m-2 p-2 border-2 border-red-500 || ${border}`} onClick={handleCreateSettlement}> Create a City 
-          <p className='text-xs font-bold pt-2'> Costs 10 Gold and must have a Population of 10 or more</p>
+        <button className={`m-2 p-2 border-2 border-red-500 || ${border}`} onClick={handleCreateCity}> Create a City 
+          <p className='text-xs font-bold pt-2'> Costs 20 Gold and must have a Population of 10 or more</p>
         </button>
         
       </div>
       <div className='flex flex-wrap m-auto justify-around p-5'>
         <div>
-          <button className={`m-2 p-2 border-2 border-red-500`}> Research Farming </button>
+        <img style={{ margin: 'auto', height: '40px', width: '40px'}}src='https://gamepedia.cursecdn.com/civ6_gamepedia_en/9/92/Icon_building_granary.png?version=46862d8d4dc00a68524d3251b1dee735' alt='farm'/>
+        <h2> Farms {farm} </h2>
+          <button className={`m-2 p-2 border-2 border-red-500 || ${farmBorder}`} onClick={handleCreateFarm} disabled={buttonDisable}> Build Farm 
+          <p className='text-xs font-bold pt-2'> Costs 15 Gold and must have a Population of 30 or more</p>
+          <p className='text-xs font-bold pt-2 text-red-500'>{message}</p>
+          </button>
         </div>
         <div>
-          <button className={`m-2 p-2 border-2 border-red-500`}> Research Pottery </button>
-        </div>
-        <div>
-          <button className={`m-2 p-2 border-2 border-red-500`}> Research Mining </button>
+        <img style={{ margin: 'auto', height: '40px', width: '40px'}}src='https://vignette.wikia.nocookie.net/civilization/images/4/48/Mine_%28Civ6%29.png/revision/latest/top-crop/width/720/height/900?cb=20190812195142' alt='farm'/>
+          <h2> Mines {mine} </h2>
+          <button className={`m-2 p-2 border-2 border-red-500`} onClick={handleCreateMine} disabled={buttonDisable}> Build Mine 
+          <p className='text-xs font-bold pt-2'> Costs 30 Gold and Requires 2 Cities</p>
+          <p className='text-xs font-bold pt-2 text-red-500'>{message}</p>
+          </button>        
         </div>
       </div>
+    </div>
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            <header className='w-full bg-gray-900 px-6'>
+                <div className='container mx-auto py-6 flex flex-wrap justify-center items-center text-gray-100'>
+                    Manage Your Own Civilization!
+                </div>
+            </header>
+        </div>
+    )
+}
+
+export default Header
